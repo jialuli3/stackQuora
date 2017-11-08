fbb27468422be05c734ca03524762ba1586c358e
jialuli3 <lijialudew@gmail.com>
2017-11-07 21:36:31 -0600

    my question display done

--- src/pages/my-questions-answers/my-questions-answers.ts
+++ src/pages/my-questions-answers/my-questions-answers.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StackMockProvider} from '../../providers/stack-mock/stack-mock'
 
 /**
  * Generated class for the MyQuestionsAnswersPage page.
@@ -14,12 +15,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'my-questions-answers.html',
 })
 export class MyQuestionsAnswersPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  hide_trash_button:false;
+  contents: any;
+  hide_post:Array<boolean> =[false,false,false,false,false,false,false,false,false,false];
+  constructor(public navCtrl: NavController, public navParams: NavParams,public mockData: StackMockProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MyQuestionsAnswersPage');
+    this.mockData.load_contents().subscribe(data=>{
+      this.contents=data.contents;
+      console.log(this.contents)
+    });
   }
-
+  show_deletion(){
+    console.log("show deletion")
+    if(this.hide_trash_button==true){
+      this.hide_trash_button=false;
+    }
+    else{
+      this.hide_trash_button=true;
+    }
+    console.log(this.hide_trash_button)
+  }
+  delete_post(i){
+    this.hide_post[i]="true";
+    //delete question/answer/API
+  }
 }
